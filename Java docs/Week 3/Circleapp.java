7c4388907202a3ef8473c1a2931f899aa0706444
AKAASH SAINI <57020330+akaaash101@users.noreply.github.com>
2023-01-23 12:31:45 -0500

    Add files via upload

---
+++ Java docs/Week 3/Circleapp.java
@@ -0,0 +1,12 @@
+public class Circleapp {
+
+    public static void main(String args[]){
+
+        Circle c1= new Circle(radius:3,color:"red");
+        Circle c2= new Circle(radius:3,color:"red");
+
+        System.out.println(c1.equals(c2));
+
+        
+    }
+}
