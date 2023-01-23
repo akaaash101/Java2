e3b90eab7ef9bc7c1ff98112e090a52c67c90193
AKAASH SAINI <57020330+akaaash101@users.noreply.github.com>
2023-01-23 12:31:01 -0500

    Add files via upload

---
+++ week_2/CourseApp.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/**
+ * CourseApp
+ */
+public class CourseApp {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int[] grades = new int[5];
+            // declare and create an array of type Course
+            // new to create array
+            CourseApp[] courses = new CourseApp[3];
+            // create objects
+            /*
+             * for (int i = 0; i < courses.length; i++)
+             * courses[i] = new Course();
+             * 
+             * System.out.println("Enter the following information");
+             * 
+             * for (int i = 0; i < courses.length; i++){
+             * System.out.print("ID: ");
+             * int id= input.nextInt();
+             * System.out.print("Name: ");
+             * String name= input.next();
+             * courses[i].setId(id);
+             * courses[i].setName(name);
+             * }
+             */
+            // using constructor with arguments:
+            for (int i = 0; i < courses.length; i++) {
+                System.out.print("ID: ");
+                int id = input.nextInt();
+                System.out.print("Name: ");
+                String name = input.next();
+                courses[i] = new CourseApp();
+            }
+
+            for (int i = 0; i < courses.length; i++)
+                System.out.println(courses[i]);
+        }
+
+    }
+
+}
